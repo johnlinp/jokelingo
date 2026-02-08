4080dc1f9cbf1cfc121f8a34e48c7be7a9b1f779
John Lin <johnlinp@gmail.com>
2026-02-08 11:05:51 -0800

    Allow view/edit database in admin page

---
+++ feed/admin.py
@@ -0,0 +1,90 @@
+"""
+Django admin configuration for Jokelingo models.
+"""
+
+from django.contrib import admin
+from .models import Post, User, EngagementEvent
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Admin interface for Post model."""
+    
+    list_display = [
+        'id',
+        'source_provider',
+        'source_language_code',
+        'target_language_code',
+        'status',
+        'helpful_count_cache',
+        'confusing_count_cache',
+        'created_at',
+    ]
+    
+    list_filter = [
+        'status',
+        'source_provider',
+        'source_language_code',
+        'target_language_code',
+        'created_at',
+    ]
+    
+    search_fields = [
+        'id',
+        'translation_text',
+        'explanation_text',
+        'source_raw_url',
+        'source_canonical_url',
+    ]
+    
+    readonly_fields = [
+        'id',
+        'created_at',
+        'updated_at',
+    ]
+    
+    fieldsets = (
+        ('Identity', {
+            'fields': ('id', 'status', 'created_at', 'updated_at')
+        }),
+        ('Languages', {
+            'fields': ('source_language_code', 'target_language_code')
+        }),
+        ('Source', {
+            'fields': ('source_provider', 'source_raw_url', 'source_canonical_url')
+        }),
+        ('Content', {
+            'fields': ('translation_text', 'explanation_text')
+        }),
+        ('Author', {
+            'fields': ('author_user',)
+        }),
+        ('Engagement (Cached)', {
+            'fields': ('helpful_count_cache', 'confusing_count_cache'),
+        }),
+    )
+    
+    raw_id_fields = ['author_user']
+    
+    date_hierarchy = 'created_at'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Admin interface for User model."""
+    
+    list_display = ['id', 'email', 'display_name', 'created_at']
+    search_fields = ['email', 'display_name']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    date_hierarchy = 'created_at'
+
+
+@admin.register(EngagementEvent)
+class EngagementEventAdmin(admin.ModelAdmin):
+    """Admin interface for EngagementEvent model."""
+    
+    list_display = ['id', 'post', 'user', 'engagement_type', 'created_at']
+    list_filter = ['engagement_type', 'created_at']
+    search_fields = ['post__id', 'user__email']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    raw_id_fields = ['post', 'user']
