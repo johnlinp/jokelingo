5a4b29859cc2de60ccea0b2fb2f6cb39e20dc730
John Lin <johnlinp@gmail.com>
2026-02-08 11:06:18 -0800

    Switch to PostgreSQL

--- jokelingo/settings.py
+++ jokelingo/settings.py
@@ -4,6 +4,7 @@ Django settings for jokelingo project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,11 @@ WSGI_APPLICATION = 'jokelingo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
