90495cb75f72c47555dac869a87511f3cafea030
John Lin <johnlinp@gmail.com>
2026-02-10 20:37:43 -0800

    Store last language path in cookie

--- jokelingo/urls.py
+++ jokelingo/urls.py
@@ -15,7 +15,23 @@ def language_feed_view(request, source_language_code, target_language_code):
         'target_language_code': target_language_code,
         'canonical_url': canonical_url,
     }
-    return render(request, 'index.html', context)
+    response = render(request, 'index.html', context)
+    
+    # Store the language choice in a cookie (expires in 1 year)
+    language_path = f'/{source_language_code}/{target_language_code}/'
+    response.set_cookie('preferred_language_path', language_path, max_age=31536000)  # 1 year
+    
+    return response
+
+def root_redirect_view(request):
+    """Redirect root path based on cookie preference, defaulting to /es/en/"""
+    preferred_path = request.COOKIES.get('preferred_language_path', '/es/en/')
+    
+    # Validate that the preferred path is one of our valid language paths
+    if preferred_path not in ['/es/en/', '/fr/en/']:
+        preferred_path = '/es/en/'
+    
+    return redirect(preferred_path, permanent=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +39,5 @@ urlpatterns = [
     path('api/v1/', include('feed.urls')),
     path('es/en/', lambda request: language_feed_view(request, 'es', 'en'), name='es_en_index'),
     path('fr/en/', lambda request: language_feed_view(request, 'fr', 'en'), name='fr_en_index'),
-    path('', lambda request: redirect('/es/en/', permanent=False), name='root_redirect'),
+    path('', root_redirect_view, name='root_redirect'),
 ]
