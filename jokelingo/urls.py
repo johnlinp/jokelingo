89f4bc50121bb2c46e53deb8993eb51dba28ab83
John Lin <johnlinp@gmail.com>
2026-02-10 20:28:22 -0800

    Redirect / to /es/en/

--- jokelingo/urls.py
+++ jokelingo/urls.py
@@ -4,10 +4,12 @@ URL configuration for jokelingo project.
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('api/v1/', include('feed.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('es/en/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', lambda request: redirect('/es/en/', permanent=False), name='root_redirect'),
 ]
